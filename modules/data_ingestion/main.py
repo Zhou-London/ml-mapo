a7680d78906a4d63553ab1e1125af558ec418211
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2026-03-16 15:38:45 +0000

    Support FX.

--- modules/data_ingestion/main.py
+++ modules/data_ingestion/main.py
@@ -4,14 +4,20 @@ import yfinance
 
 from enum import Enum
 
+
 # Structs
 class AssetEnum(Enum):
     Equity = 1
     Crypto = 2
+    CommodityFutrues = 3
+    Bond = 4
+    FX = 5
+
 
 class CurrencyEnum(Enum):
     USD = 1
     GBP = 2
+
 
 class Portfolio(pydantic.BaseModel):
     symbol: list[str] = []
@@ -22,13 +28,21 @@ class Portfolio(pydantic.BaseModel):
     def __str__(self):
         lines = [f"Portfolio(size={self.size()})"]
         for i in range(self.size()):
-            lines.append(f"  {self.symbol[i]} | {self.assetType[i].name} | {self.currencyType[i].name} | {self.amount[i]}")
+            lines.append(
+                f"  {self.symbol[i]} | {self.assetType[i].name} | {self.currencyType[i].name} | {self.amount[i]}"
+            )
         return "\n".join(lines)
 
     def size(self):
         return len(self.symbol)
-    
-    def push_back(self, symbol:str, assetType: AssetEnum, currencyType: CurrencyEnum, amount: float):
+
+    def push_back(
+        self,
+        symbol: str,
+        assetType: AssetEnum,
+        currencyType: CurrencyEnum,
+        amount: float,
+    ):
         self.symbol.append(symbol)
         self.assetType.append(assetType)
         self.currencyType.append(currencyType)
@@ -70,6 +84,7 @@ class AssetData(pydantic.BaseModel):
             last_date = self.date[-1]
             gap = (date - last_date).days
             if gap > 1:
+                # Use the close price of most recent day
                 last_close = self.close[-1]
                 for i in range(1, gap):
                     fill_date = last_date + datetime.timedelta(days=i)
@@ -86,30 +101,16 @@ class AssetData(pydantic.BaseModel):
         self.close.append(close)
         self.volume.append(volume)
 
+
 # Main
 def main():
 
     # User input
     portfolio = Portfolio()
-    portfolio.push_back(
-        "AAPL",
-        AssetEnum.Equity,
-        CurrencyEnum.USD,
-        1.0
-    )
-    portfolio.push_back(
-        "GOOGL",
-        AssetEnum.Equity,
-        CurrencyEnum.USD,
-        1.0
-    )
-    portfolio.push_back(
-        "HSBA",
-        AssetEnum.Equity,
-        CurrencyEnum.GBP,
-        1.0
-    )
-
+    portfolio.push_back("AAPL", AssetEnum.Equity, CurrencyEnum.USD, 1.0)
+    portfolio.push_back("GOOGL", AssetEnum.Equity, CurrencyEnum.USD, 1.0)
+    portfolio.push_back("HSBA", AssetEnum.Equity, CurrencyEnum.GBP, 1.0)
+    portfolio.push_back("GBP", AssetEnum.FX, CurrencyEnum.USD, 100.0)
 
     PERIOD = "5d"
     print(portfolio)
@@ -118,13 +119,15 @@ def main():
     assetDataTable: list[AssetData] = []
 
     for i in range(portfolio.size()):
-        if(portfolio.assetType[i] == AssetEnum.Equity):
-            if(portfolio.currencyType[i] == CurrencyEnum.USD):
+        if portfolio.assetType[i] == AssetEnum.Equity:
+            if portfolio.currencyType[i] == CurrencyEnum.USD:
                 try:
                     dataframe = yfinance.Ticker(portfolio.symbol[i])
                     history = dataframe.history(period=PERIOD)
                 except Exception as e:
-                    print(f"[ERROR]: yfinance failed to fetch data for {portfolio.symbol[i]}: {e}")
+                    print(
+                        f"[ERROR]: yfinance failed to fetch data for {portfolio.symbol[i]}: {e}"
+                    )
                     continue
 
                 assetData = AssetData(symbol=portfolio.symbol[i])
@@ -139,16 +142,19 @@ def main():
                     )
                 assetDataTable.append(assetData)
 
-            elif(portfolio.currencyType[i] == CurrencyEnum.GBP):
+            elif portfolio.currencyType[i] == CurrencyEnum.GBP:
                 try:
-                    FX: float = 1.35 # ! GBP -> USD
-                    multiplier: float = 0.01 # Pennies -> Pounds
+                    ticker = yfinance.Ticker("GBPUSD=X")  # GBP -> USD
+                    FX: float = ticker.info.get("regularMarketPrice")
+                    multiplier: float = 0.01  # Pennies -> Pounds
 
                     tickerName = portfolio.symbol[i] + ".L"
                     dataframe = yfinance.Ticker(tickerName)
                     history = dataframe.history(period=PERIOD)
                 except Exception as e:
-                    print(f"[ERROR]: yfinance failed to fetch data for {portfolio.symbol[i]}: {e}")
+                    print(
+                        f"[ERROR]: yfinance failed to fetch data for {portfolio.symbol[i]}: {e}"
+                    )
                     continue
 
                 assetData = AssetData(symbol=portfolio.symbol[i])
@@ -162,9 +168,45 @@ def main():
                         volume=int(row["Volume"]),
                     )
                 assetDataTable.append(assetData)
+            else:
+                print(
+                    f"[ERROR]: Currency not supported for {portfolio.assetType[i]}: {portfolio.currencyType[i]}"
+                )
+                continue
+
+        elif portfolio.assetType[i] == AssetEnum.FX:
+            if portfolio.currencyType[i] == CurrencyEnum.USD:
+                try:
+                    tickerName = portfolio.symbol[i] + "USD=X"
+                    dataframe = yfinance.Ticker(tickerName)
+                    history = dataframe.history(period=PERIOD)
+                except Exception as e:
+                    print(
+                        f"[ERROR]: yfinance failed to fetch data for {portfolio.symbol[i]}: {e}"
+                    )
+                    continue
+
+                assetData = AssetData(symbol=portfolio.symbol[i])
+                for date, row in history.iterrows():
+                    assetData.push_back(
+                        date=date.date(),
+                        open=row["Open"],
+                        high=row["High"],
+                        low=row["Low"],
+                        close=row["Close"],
+                        volume=int(row["Volume"]),
+                    )
+                assetDataTable.append(assetData)
+
+        else:
+            print(
+                f"[ERROR]: Asset not supported for {portfolio.assetType[i]}: Currency {portfolio.currencyType[i]}"
+            )
+            continue
 
     for i in range(len(assetDataTable)):
         print(assetDataTable[i])
+
 
 if __name__ == "__main__":
     main()
