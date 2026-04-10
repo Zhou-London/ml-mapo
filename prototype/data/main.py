677286de144376a7038136c5bd4390703c1bc39a
zhouzhou <zhouzhou.zhang.24@ucl.ac.uk>
2026-04-10 13:04:12 +0100

    add psycopg2, define the structure of data module

--- prototype/data/main.py
+++ prototype/data/main.py
@@ -1,12 +1,20 @@
 from time import sleep
 
 import zmq
+import yfinance
+
+target_tickers = ["AAPL", "GOOG", "AMZN", "MSFT", "TSLA", "META", "NVDA"]
 
 # Create socket
-def connect() -> zmq.Socket:
+def connect_socket() -> zmq.Socket:
     ctx = zmq.Context()
     pub = ctx.socket(zmq.PUB)
     pub.bind("tcp://*:5555")
     return pub
 
+# Connect to TimescaleDB
+def connect_database():
+    # placeholder
+
 def main():
+    # Get tickers's past 12 month data and store into DB
